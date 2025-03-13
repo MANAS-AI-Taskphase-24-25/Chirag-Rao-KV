9ce1594ab53940976f252ddb0e0116e1e93bb562
chiragraokv <chiragrao.kv@gmail.com>
2025-03-13 19:54:47 +0530

    vid upload

--- Task 6.0/trackball.py
+++ Task 6.0/trackball.py
@@ -15,7 +15,35 @@ def circularity(area,perimeter):
     else:
         return 0
     
+
+# this function is used to find if the bounding rectangle have specific ration in thir height and width. Adjusted to fit the approximate ratio of torso of players
+def IsRectangle(l,b):
+    ratio = l/b
+    if 0.7<ratio<=3:
+        return 1
+    else: return 0
     
+def FindPlayer(image,upper,lower,name):
+    hsv = cv.cvtColor(image,cv.COLOR_BGR2HSV)
+    blur = cv.GaussianBlur(hsv,(7,7),0)
+    mask = cv.inRange(blur,lower, upper)
+    cv.imshow(f"{name} mask",mask)
+    mask = cv.erode(mask,(5,5),iterations= 10)
+    mask = cv.dilate(mask,(5,5),iterations=15)
+    cv.imshow(f"{name} mask",mask)
+    player = []
+    
+    contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        #using the area and ratio between height and width of bounding rectangle to find players.
+        if 2000<cv.contourArea(c)<2400:
+            
+            x,y,w,h = cv.boundingRect(c)
+            if IsRectangle(h,w):
+                print(f"found player {cv.boundingRect(c)}")
+                player.append((x,y,w,h))
+            
+    return player
 
 
 def FindYellowball(image):
@@ -56,17 +84,24 @@ def FindYellowball(image):
         print("No ball detected.")
         return []
     
-while(True):
+while vid.isOpened():
     _,frame = vid.read()
     coordinates= np.array(FindYellowball(frame))
-    if coordinates.all():  # Ensure x and y are not empty
-        print(f"coordinates are {coordinates}")
+    lower = np.array([10, 60, 80])  
+    upper= np.array([30, 255, 255])
+    team1 = FindPlayer(frame,upper,lower,"Yellow Team")
+    #team2 = FindPlayer(frame,upper_red,lower_red,"Red team")
+    
+    for player in team1:
+       x1,y1,h1,w1 = player
+       cv.rectangle(frame,(x1,y1),(x1+w1,y1+h1),(255,255,255),2)
+
+    if coordinates.all(): 
         cv.circle(frame, coordinates, 5, (0, 0, 255), 20)  
         cv.circle(frame, coordinates, 5, (0, 255, 0), 10)  
         cv.imshow("circled",frame)
         out.write(frame)
-
-    
+        
     k = cv.waitKey(5) & 0xFF
     if k == 27:
         break
